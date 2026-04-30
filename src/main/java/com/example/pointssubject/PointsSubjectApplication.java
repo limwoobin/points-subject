5e727875a6e63cc13e751b2de5b671380a7d40bb
limwoobin <drogba02@naver.com>
2026-05-01 01:12:12 +0900

    project init

---
+++ src/main/java/com/example/pointssubject/PointsSubjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.pointssubject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointsSubjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointsSubjectApplication.class, args);
+	}
+
+}
